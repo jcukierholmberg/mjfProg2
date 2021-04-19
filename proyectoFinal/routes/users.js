afa190957b21ecf8a9f1db97ecb090ee100ebde2
martinatriulzi <martinatriulzi@gmail.com>
2021-04-19 15:44:06 -0300

    eve

--- proyectoFinal/routes/users.js
+++ proyectoFinal/routes/users.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/', usersController.profile);
+router.get('/profile', usersController.profile);
 
 router.get('/profileEdit', usersController.profileEdit);
 
