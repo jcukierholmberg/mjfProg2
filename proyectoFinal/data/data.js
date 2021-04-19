325cfd59cc3f98705567a143ac5777b114139b49
martinatriulzi <mtriulzi@udesa.edu.ar>
2021-04-19 17:45:45 -0300

    dataa

--- proyectoFinal/data/data.js
+++ proyectoFinal/data/data.js
@@ -1,15 +1,198 @@
-let buzosgi = {
+
+let buzos = {
     lista: [ 
     { id:0,
-    nombreDelInstrumento: "Trompeta",
-    marca: "Yamaha",
-    modelo: "K-40",
-    fechaDeCreacion: "2/3/2021",
-    imagen: "https://ar.xprostore.com/image/cache/data/imagenesweb/ytr2330-650x650.jpg" ,
-    descripcion: 'Instrumento musical de viento , consistente en un tubo largo de metal que va ensanchándose desde la boquilla al pabellón y produce diversidad de sonidos según la fuerza con que la boca impele el aire.',
-    creadoPor: 'frizzi',
-    precio: 100,
-    comentarios: [{usuarioQueComento: 'mheber', comentario:'Bueno'}, {usuarioQueComento: 'mlang', comentario:'Tremendo'}, {usuarioQueComento: 'frizzi', comentario:'Malo'}, {usuarioQueComento: 'tmasic', comentario:'Increible'}, {usuarioQueComento: 'iromani', comentario:'Estaba Roto'}]
+    marca: "Adidas",
+    modelo: "Original 3 stripes",
+    talles:"M,L",
+    fechaDeCreacion: "2/7/2019",
+    imagen: "/images/buzoadidass.jpg" ,
+    descripcion: "Buzo Negro Adidas Originals 3-Stripes Crew. Tela Algodón|Polyester|Lino|Viscosa Composición	77% algodón – 23% poliéster",
+    CreadoPor: 'joaquingonzalez',
+    precio: 65,
+    enOferta: 'si',
+    comentarios: [
+        {usuarioQueComento: 'flormartinez', comentario:'Comodisimo!'}, 
+        
+        {usuarioQueComento: 'martinasanchez',
+        comentario:'Se me descoloro con el lavado'}, 
+        
+        {usuarioQueComento: 'mariagomez', 
+        comentario:'Estaba sucio'}, 
+        
+        {usuarioQueComento: 'emiliaaramburu', comentario:'Increible, divino'}, 
+        
+        {usuarioQueComento: 'gonzaloecheverria', comentario:'Muy canchero'}]
 },
-    ]
-}
+    { id:1,
+    marca: "All Saints",
+    modelo: "Grey confy",
+    talles:"S,M,L",
+    fechaDeCreacion: "12/5/2020",
+    imagen: "/images/allsaintsbuzo.jpg" ,
+    descripcion: "Buzo escote redondo y manga ranglan. Color gris. Cintura y puños elastizados. Talle de prenda de la foto: L",
+    CreadoPor: 'gonzaloecheverria',
+    precio: 115,
+    enOferta: "no",
+    comentarios: [
+        {usuarioQueComento: 'flormartinez', 
+        comentario:'Se lo regale a mi novio! Le quedo perfecto'}, 
+        
+        {usuarioQueComento: 'martinasanchez', comentario:'Lindisimo'}, 
+        
+        {usuarioQueComento: 'mariagomez', 
+        comentario:'Tardo mucho en llegar el pedido'}, 
+        
+        {usuarioQueComento: 'emiliaaramburu', 
+        comentario:'Se elastiza'}, 
+        
+        {usuarioQueComento: 'joaquingonzalez', comentario:'Muy comodo'}]
+},
+
+    { id:3,
+    marca: "Fila",
+    modelo: "Capucha tres colores",
+    talles:"S,M",
+    fechaDeCreacion: "13/12/2008",
+    imagen: "/images/buzofila.jpg" ,
+    descripcion: "Buzo rojo Fila net. Super comodo. Es abrigado",
+    CreadoPor: 'flormartinez',
+    precio: 50,
+    enOferta: "si",
+    comentarios: [
+        {usuarioQueComento: 'gonzaloecheverria', comentario:'Comodisimo'},
+
+        {usuarioQueComento: 'martinasanchez', comentario:'Cancherisimo'}, 
+
+        {usuarioQueComento: 'mariagomez', 
+        comentario:'Se confundieron de talle'}, 
+
+        {usuarioQueComento: 'emiliaaramburu', 
+        comentario:'El rojo se decoloro'}, 
+
+        {usuarioQueComento: 'joaquingonzalez', comentario:'Lo uso para todo!'}]
+ },
+
+    { id:4,
+    marca: "Gucci",
+    modelo: "Cierre con sigla G",
+    talles:"S,L",
+    fechaDeCreacion: "28/1/2020",
+    imagen: "/images/gucci2buzo.jpg" ,
+    descripcion: "Buzo blanco con detalles originales Gucci.",
+    CreadoPor: 'martinasanchez',
+    precio: 150,
+    enOferta: 'no',
+    comentarios: [
+        {usuarioQueComento: 'gonzaloecheverria', comentario:'Muy caro, no lo vale'}, 
+
+        {usuarioQueComento: 'flormartinez', comentario:'Cancherisimo'}, 
+
+        {usuarioQueComento: 'mariagomez', 
+        comentario:'Divino el color'}, 
+
+        {usuarioQueComento: 'emiliaaramburu', comentario:'Super'}, 
+
+        {usuarioQueComento: 'joaquingonzalez', comentario:'Muy suave'}]
+},
+
+    { id:5,
+    marca: "HM",
+    modelo: "Bicolor con capucha",
+    talles:"S",
+    fechaDeCreacion: "19/3/2005",
+    imagen: "images/hm2buzo.jpg" ,
+    descripcion: "Sudadera en mezcla de algodón con capucha cruzada con forro de punto y cordón de ajuste, un bolsillo canguro y puños y bajo en punto elástico de canalé. Tejido cepillado suave en el interior",
+    CreadoPor: 'mariagomez',
+    precio: 40,
+    enOferta: 'si',
+    comentarios: [
+        {usuarioQueComento: 'gonzaloecheverria', comentario:'Super'}, 
+
+        {usuarioQueComento: 'joaquingonzalez', comentario:'Mala calidad'}, 
+
+        {usuarioQueComento: 'martinasanchez', 
+        comentario:'Se descoloro'}, 
+
+        {usuarioQueComento: 'emiliaaramburu', comentario:'Comodisimo'}, 
+
+        {usuarioQueComento: 'flormartinez', 
+        comentario:'Todo terreno'}]
+},
+
+    {id:6,
+    marca: "Prada",
+    modelo: "Bicolor sin capucha",
+    talles:"M",
+    fechaDeCreacion: "2/11/2015",
+    imagen: "/images/prada2buzo.jpg" ,
+    descripcion: "Sudadera Prada bicolor: negro y azul. Tiene detalles en el dorso",
+    CreadoPor: 'flormartinez',
+    precio: 40,
+    enOferta: 'si',
+    comentarios: [
+        {usuarioQueComento: 'gonzaloecheverria', comentario:'Tremendo'}, 
+
+        {usuarioQueComento: 'joaquingonzalez', comentario:'Muy suave'}, 
+
+        {usuarioQueComento: 'martinasanchez', 
+        comentario:'Muy apretado'}, 
+
+        {usuarioQueComento: 'emiliaaramburu', 
+        comentario:'El pedido tardo mucho'}, 
+
+        {usuarioQueComento: 'mariagomez', 
+        comentario:'Lindisimos colores'}]
+},
+
+    {id:7,
+    marca: "Nike",
+    modelo: "Bicolor contra lluvia",
+    talles:"S,L",
+    fechaDeCreacion: "24/12/2018",
+    imagen: "/images/nikebuzo.jpg" ,
+    descripcion: "Buzo sin capucha. Material poliester. Super comodo",
+    CreadoPor: 'gonzaloecheverria',
+    precio: 55,
+    enOferta: 'si',
+    comentarios: [
+        {usuarioQueComento: 'flormartinez', comentario:'Super abrigado'}, 
+
+        {usuarioQueComento: 'joaquingonzalez', comentario:'Colores super'}, 
+
+        {usuarioQueComento: 'martinasanchez', 
+        comentario:'Re suave'}, 
+
+        {usuarioQueComento: 'emiliaaramburu', 
+        comentario:'Tela muy finita'}, 
+
+        {usuarioQueComento: 'mariagomez', 
+        comentario:'Tremendo'}]
+},
+
+    {id:8,
+    marca: "Zara",
+    modelo: "Bicolor sin capucha amarillo",
+    talles:"M",
+    fechaDeCreacion: "21/8/2018",
+    imagen: "/images/zarabuzo.jpg" ,
+    descripcion: "Buzo sin capucha. Material poliester. Super comodo y abrigado. Colores unicos",
+    CreadoPor: 'martinasanchez',
+    precio: 60,
+    enOferta: 'no',
+    comentarios: [
+        {usuarioQueComento: 'flormartinez', comentario:'Comodisimo y muy calentito'}, 
+
+        {usuarioQueComento: 'joaquingonzalez', comentario:'Colores super combinables'}, 
+
+        {usuarioQueComento: 'gonzaloecheverria', 
+        comentario:'Suave'}, 
+
+        {usuarioQueComento: 'emiliaaramburu', 
+        comentario:'El talle me apreto'}, 
+
+        {usuarioQueComento: 'mariagomez', 
+        comentario:'Tremendo'}]
+},
+
+]}
