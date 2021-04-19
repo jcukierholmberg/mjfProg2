07c0a762257d72a023a1bffb13916c096d5d2685
martinatriulzi <mtriulzi@udesa.edu.ar>
2021-04-19 17:01:48 -0300

    data

--- proyectoFinal/controller/indexController.js
+++ proyectoFinal/controller/indexController.js
@@ -7,7 +7,25 @@ let indexController = {
         return res.render ('search')
     },
     
-    } ;
+   
 
+    searchResults: (req, res) => {
+        let search = req.query.searchResults
+
+        let result = []
+        for( let i = 0; i < instrumentos.lista.length; i++){
+            var nombreMarcaModelo = instrumentos.lista[i].nombreDelInstrumento.toLowerCase() + ' ' + instrumentos.lista[i].marca.toLowerCase() + ' ' + instrumentos.lista[i].modelo.toLowerCase()
+            var nombreModelo = instrumentos.lista[i].nombreDelInstrumento.toLowerCase() + ' ' + instrumentos.lista[i].modelo.toLowerCase()
+            if(instrumentos.lista[i].nombreDelInstrumento.toLowerCase().includes(search.toLowerCase()) || instrumentos.lista[i].marca.toLowerCase().includes(search.toLowerCase()) || instrumentos.lista[i].modelo.toLowerCase().includes(search.toLowerCase())){
+                result.push(instrumentos.lista[i])
+            }
+            else if(nombreMarcaModelo.includes(search.toLowerCase()) || nombreModelo.includes(search.toLowerCase())){
+                result.push(instrumentos.lista[i])
+            }
+        }
+        res.render('search-results', {resultadoSearch: result, instrumentitos: instrumentos.lista, parametroSearch: search})
+    },
+
+};
     module.exports = indexController;
     
---
+++ proyectoFinal/data/data.js
@@ -0,0 +1,15 @@
+let buzosgi = {
+    lista: [ 
+    { id:0,
+    nombreDelInstrumento: "Trompeta",
+    marca: "Yamaha",
+    modelo: "K-40",
+    fechaDeCreacion: "2/3/2021",
+    imagen: "https://ar.xprostore.com/image/cache/data/imagenesweb/ytr2330-650x650.jpg" ,
+    descripcion: 'Instrumento musical de viento , consistente en un tubo largo de metal que va ensanchándose desde la boquilla al pabellón y produce diversidad de sonidos según la fuerza con que la boca impele el aire.',
+    creadoPor: 'frizzi',
+    precio: 100,
+    comentarios: [{usuarioQueComento: 'mheber', comentario:'Bueno'}, {usuarioQueComento: 'mlang', comentario:'Tremendo'}, {usuarioQueComento: 'frizzi', comentario:'Malo'}, {usuarioQueComento: 'tmasic', comentario:'Increible'}, {usuarioQueComento: 'iromani', comentario:'Estaba Roto'}]
+},
+    ]
+}
