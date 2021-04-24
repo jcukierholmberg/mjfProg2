ff834e1510e9f3eeb6d1fa710209740f5672f8d4
Apple <apple@Joaco-cukier.local>
2021-04-24 15:14:05 -0300

    kasjd

--- proyectoFinal/routes/products.js
+++ proyectoFinal/routes/products.js
@@ -5,7 +5,7 @@ const productsController = require ("../controller/productsController");
 
 router.get('/:id', productsController.product);
 
-router.get('/productAdd/:id', productsController.productAdd);
+router.get('/productAdd', productsController.productAdd);
 
 router.get ("/allProducts/:id", productsController.allProducts);
 
